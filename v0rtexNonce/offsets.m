c4d7d2d42b0ff5441c469ded714ea14949be21a5
arinc9 <arinc9@windowslive.com>
2017-12-21 23:01:43 +0300

    i7+ 10.2, 10.1.1

--- v0rtexNonce/offsets.m
+++ v0rtexNonce/offsets.m
@@ -1522,7 +1522,24 @@ void load_offsets(void)
     		LOG("10.3 - 14E277 offsets not found for %s", device);
     		exit(1);
     	}
-
+        
+        //10.2, 10.1.1
+    	if(!strcmp(version, "14B150") || !strcmp(version, "14B100") || !strcmp(version, "14C92"))
+    	{
+    		OFFSET_ZONE_MAP                             = 0xfffffff007558478;
+    		OFFSET_KERNEL_MAP                           = 0xfffffff0075b4050;
+    		OFFSET_KERNEL_TASK                          = 0xfffffff0075b4048;
+    		OFFSET_REALHOST                             = 0xfffffff00753aba0;
+    		OFFSET_BZERO                                = 0xfffffff00708df80;
+    		OFFSET_BCOPY                                = 0xfffffff00708ddc0;
+    		OFFSET_COPYIN                               = 0xfffffff00718d3a8;
+    		OFFSET_COPYOUT                              = 0xfffffff00718d59c;
+    		OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff0070a611c;
+    		OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070b9374;
+    		OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff0070a5c40;
+    		OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006eee1b8;
+    		OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff0064b5174;
+    	}
 
     }
 
